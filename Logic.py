964ae43888183720a9732adc09e8856ea974fa11
Mikhub1 <122497460+Mikhub1@users.noreply.github.com>
2023-01-16 18:30:33 -0330

    Basic logic test

---
+++ Logic.py
@@ -0,0 +1,30 @@
+#task = True
+#clarity = False
+# if not task or clarity:
+#     print("Task not completed")
+# elif task and clarity
+#     print("clarity unsolved")  
+# else:    
+#     print("Task done")
+
+
+
+start_value = "Tunfref"
+cycles = 0
+for iter in start_value:
+    cycles += 1
+    start_value = start_value.replace('f',"d")
+    start_value = start_value.replace('T','H')
+print(cycles)
+print(start_value)
+
+
+# if "ten" == 10:
+#     print("Less likely")
+# elif "primate" > "sapiens" or "chicken" > "aves":
+#     print("Zero")
+# else:
+#     print("End")   
+
+
+
